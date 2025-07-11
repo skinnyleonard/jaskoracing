b8a8a8ede34a0ef118966bd551003fdf6ba15ff0
Jesus Fernandez <bachos2007@hotmail.com>
2025-07-10 23:32:16 -0300

    realizamos el primer paso del cuerpo del auto

--- core/src/main/java/io/github/jasko/Main.java
+++ core/src/main/java/io/github/jasko/Main.java
@@ -1,12 +1,26 @@
 package io.github.jasko;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import screens.PlayScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
 	
+	
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+    	setScreen(new PlayScreen());
+    }
+    
+    @Override
+    public void render() {
+    	super.render();
+    }
+    
+    @Override
+    public void dispose() {
+    	super.dispose();
     }
 }
---
+++ core/src/main/java/screens/Constants.java
@@ -0,0 +1,11 @@
+package screens;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Constants {
+	private Constants() {}
+	
+	public static final Vector2 GRAVITY = new Vector2(0, 0);
+	public static final float DEFUALT_ZOOM = 6f;
+	public static final float PPM = 50.0f;
+}
