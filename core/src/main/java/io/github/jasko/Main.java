055622a70b566d177ec277ce11462cce9f4b5f62
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2025-11-26 10:58:30 -0300

    no usages de main
    Updated package name and modified screen handling in the Main class.

--- core/src/main/java/io/github/jasko/Main.java
+++ core/src/main/java/io/github/jasko/Main.java
@@ -1,26 +1,31 @@
-package io.github.jasko;
+package io.github.libgdxsnes;
 
 import com.badlogic.gdx.Game;
-
-import screens.MenuScreen;
-import screens.PlayScreen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import pantallas.GameScreen;
+import pantallas.MenuScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    private SpriteBatch batch;
+    private GameScreen gameScreen;
 
     @Override
     public void create() {
-//        setScreen(new PlayScreen());
-        setScreen(new MenuScreen(this));
+        batch = new SpriteBatch();
+//        gameScreen = new GameScreen(this, batch, "lanciadeltahf", "jesus");
+//        setScreen(gameScreen);
+        setScreen(new MenuScreen());
+//        setScreen(new EndingScreen(this));
     }
 
     @Override
     public void render() {
-    	super.render();
+        super.render();
     }
 
     @Override
     public void dispose() {
-    	getScreen().dispose();
+        getScreen().dispose();
     }
 }
